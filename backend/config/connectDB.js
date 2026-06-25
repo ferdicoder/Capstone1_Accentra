099f663ba3859869c0c4aaf6648cfc54604a6ceb
ced <cedrickced820@gmail.com>
2026-06-25 23:18:36 +0800

    add: supabase config

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,18 +2,17 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 
+// host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   ssl: { rejectUnauthorized: false }
 
-const pool = new Pool({ 
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: false }
- }); 
+const pool = new Pool({ connectionString: process.env.DATABASE_URI}); 
   
 const connectDB = async () =>{
   const connect = await pool.query('SELECT NOW()');
-  console.log(`Database Connected, running on PORT: ${process.env.PGPORT}, ${connect.rows[0].now}`); 
+  console.log(`Database Connected, ${connect.rows[0].now}`); 
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors'
 import { connectDB } from './config/connectDB.js';
-import apiMock from './routes/api.mock.js';
 
 const PORT = process.env.PORT;
 const server = express(); 
@@ -17,7 +16,6 @@ server.get('/', (req, res) =>{
 async function startServer(){
   try{
     await connectDB(); 
-    server.use('/mock', apiMock);
     server.listen(PORT, ()=>{
       console.log(`Server running on PORT: ${PORT}`); 
     }); 
