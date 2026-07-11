289eca316f18a9c865f24359e10430e8ee73c869
Racy <adaledres@yahoo.com>
2026-07-11 22:37:32 +0800

    Add auth layout, simplify login and signup page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,40 +1,13 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
+import { AuthLayout } from "@/components/auth-layout"
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-     
-      <div className="flex flex-col items-center justify-center bg-linear-to-br from-navy-900 via-forest-900 to-emerald-500">
-        <div className="w-fit">
-          <h1 className="text-5xl font-bold text-white mb-4">Accounting Made Simple</h1>
-          <p className="text-lg text-white">
-            Accentra bridges the gap between their accounting firms and their clients  <br />
-            a centralized platform for managing you accounting service, client documents, and BIR Compliance <br />
-            in one place. 
-          </p>
-          <div>
-          </div>
-        </div>
-      </div>
-
-       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <span>A</span>
-            </div>
-            Accentra
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-lg">
-            <LoginForm className={'w-full min-h- rounded-xl border bg-white p-8 drop-shadow-2xl'}/>
-          </div>
-        </div>
-      </div>
-      
-    </div>
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
   )
 }
+
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,37 +1,12 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { SignupForm } from "@/components/signup-form"
+import { AuthLayout } from "@/components/auth-layout"
 
 export default function SignupPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <span>A</span>
-            </div>
-            Accentra
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <SignupForm className={'shadow-md rounded-md p-4 w-100'}/>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center bg-linear-to-br from-navy-900 via-forest-900 to-emerald-500">
-        <div className="w-fit">
-          <h1 className="text-5xl font-bold text-white mb-4">Accounting Made Simple</h1>
-          <p className="text-lg text-white">
-            Accentra bridges the gap between their accounting firms and their clients  <br />
-            a centralized platform for managing you accounting service, client documents, and BIR Compliance <br />
-            in one place. 
-          </p>
-          <div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AuthLayout>
+      <SignupForm />
+    </AuthLayout>
   )
 }
