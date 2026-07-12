191d5274657f4717ce693bf0d395f89398064f29
Alexis Arcenal <aalumbis@gmail.com>
2026-07-12 19:23:51 +0800

    Implemented Registration Page step 2

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -86,12 +86,14 @@ export function LoginForm({
           className="
           mt-4
           w-full
-          h-10
-          rounded-xl
-          bg-emerald-700
-          hover:bg-forest-900
+          h-14
+          rounded-2xl
+          bg-gradient-to-r
+          from-[#0F3443]
+          to-[#10B981]
           text-white
-          transition-colors
+          hover:opacity-90
+          transition-all
           focus-visible:ring-2
           focus-visible:ring-emerald-500
           "
