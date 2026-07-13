1161798e17cc9b4ccd613593e86b2a0a27e88369
ced <cedrickced820@gmail.com>
2026-07-13 16:14:03 +0800

    revert: signup page file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 import DashboardPage from "@/pages/DashboardPage"
 
 export default function App() {
   return (
     <DashboardPage />
+=======
+import LoginPage from "@/pages/LoginPage"
+import SignupPage from "./pages/SignupPage"
+
+export default function App() {
+  return (
+    <SignupPage />
+>>>>>>> 41e3fbe (refactor: one page for registration page)
   )
 }
