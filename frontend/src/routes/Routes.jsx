071c79497614b2ce36397ae90cbb74a5b96760ee
jhanleur <jhanleurbalberan3@gmail.com>
2026-08-06 18:49:26 +0800

    feat: add client profile and request service pages

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import ClientDashboard  from '@/pages/client/ClientDashboard'; 
+import ClientDashboard from '@/pages/client/ClientDashboard';
 import LoginPage from "@/pages/client/LoginPage";
 import SignupPage from "@/pages/client/SignupPage";
+import ClientProfilePage from "@/pages/client/ClientProfilePage";
+import ClientServiceRequestsPage from "@/pages/client/ClientServiceRequestsPage";
 
 import FirmAdminDashboard from "@/pages/firm/FirmAdminDashboard";
 import FirmLoginPage from "@/pages/firm/FirmLoginPage";
@@ -23,6 +25,15 @@ export const router = createBrowserRouter([
     element: <ClientDashboard />
   },
   {
+    path: '/client/profile',
+    element: <ClientProfilePage />
+  },
+  {
+    path: '/client/service-requests',
+    element: <ClientServiceRequestsPage />
+  },
+
+  {
     path: '/firm/signin',
     element: <FirmLoginPage />
   },
@@ -38,4 +49,4 @@ export const router = createBrowserRouter([
     path: '/admin/users',
     element: <UserManagementPage />
   }
-]); 
+]);
