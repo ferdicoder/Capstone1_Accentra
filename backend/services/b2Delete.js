d4a2d90b384c26f38d32ee1bdfc0830c5639edbb
ced <cedrickced820@gmail.com>
2026-09-14 22:31:21 +0800

    config: add authorization B2 services. Use native HTTP agents for IPv4 uploads. Configure allowedfiletypes

--- backend/services/b2Delete.js
+++ backend/services/b2Delete.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { authorizeB2 } from '../config/b2Client.js';
+import { authorizeB2, b2HttpsAgent } from '../config/b2Client.js';
 
 export async function deleteFileVersion(fileName, fileId) {
   const { apiUrl, authorizationToken } = await authorizeB2();
   const res = await axios.post(
     `${apiUrl}/b2api/v3/b2_delete_file_version`,
     { fileName, fileId },
-    { headers: { Authorization: authorizationToken } }
+    { headers: { Authorization: authorizationToken }, httpsAgent: b2HttpsAgent, timeout: 15000 }
   );
   return res.data;
 }
